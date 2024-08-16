8f05f34f7bf15ca35eb627bb940ef4995c402fa1
Joao Lorenco <jllorenc00@gmail.com>
2024-08-16 19:06:10 -0300

    Sexto commit

--- src/br/com/joaolorenco/padroes/estruturais/decorator/Principal.java
+++ src/br/com/joaolorenco/padroes/estruturais/decorator/Principal.java
@@ -9,10 +9,10 @@ public class Principal {
     public static void main(String[] args) {
         
         Cafe meuCafe = new CafeSimples();
-        System.out.println(meuCafe.getDesc() + " custa: " meuCafe.getCusto());
+        System.out.println(meuCafe.getDesc() + " custa: " + meuCafe.getCusto());
         
         meuCafe = new Leite(meuCafe);
-        System.out.println(meuCafe.getDesc() + " custa " + meuCafe.getDesc();
+        System.out.println(meuCafe.getDesc() + " custa " + meuCafe.getDesc());
         
         meuCafe = new Acucar(meuCafe);
         System.out.println(meuCafe.getDesc() + " custa " + meuCafe.getDesc());
